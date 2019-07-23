90dc313c7cad47edfa5ab316888fb21750c73e64
Celine <cw089@hdm-stuttgart.de>
2019-07-23 18:18:44 +0200

    Adding methods to flask test

--- webapp/test_flaskr.py
+++ webapp/test_flaskr.py
@@ -2,5 +2,184 @@ import os
 import tempfile
 
 import pytest
+import self as self
 
 from flaskr import flaskr
+import flask
+from contextlib import contextmanager
+from flask import appcontext_pushed, g, Response
+from flask import json, jsonify, request
+
+import click
+from webapp import app
+
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['DATABASE'])
+
+## Test empty database
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    rv = client.get('/')
+    assert b'No entries here so far' in rv.data
+
+
+#Logging In and Out
+
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
+
+
+def test_login_logout(client):
+    """Make sure login and logout works."""
+
+    rv = login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'])
+    assert b'You were logged in' in rv.data
+
+    rv = logout(client)
+    assert b'You were logged out' in rv.data
+
+    rv = login(client, flaskr.app.config['USERNAME'] + 'x', flaskr.app.config['PASSWORD'])
+    assert b'Invalid username' in rv.data
+
+    rv = login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'] + 'x')
+    assert b'Invalid password' in rv.data
+
+
+## Test Adding Messages
+
+def test_messages(client):
+    """Test that messages work."""
+
+    login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'])
+    rv = client.post('/add', data=dict(
+        title='<Hello>',
+        text='<strong>HTML</strong> allowed here'
+    ), follow_redirects=True)
+    assert b'No entries here so far' not in rv.data
+    assert b'&lt;Hello&gt;' in rv.data
+    assert b'<strong>HTML</strong> allowed here' in rv.data
+
+
+app = flask.Flask(__name__)
+
+with app.test_request_context('/?name=Peter'):
+    assert flask.request.path == '/'
+    assert flask.request.args['name'] == 'Peter'
+
+
+with app.test_request_context('/?name=Peter'):
+    app.preprocess_request()
+
+with app.test_request_context('/?name=Peter'):
+    resp = Response('...')
+    resp = app.process_response(resp)
+
+
+## Faking Resources and Context
+
+def get_user():
+    user = getattr(g, 'user', None)
+    if user is None:
+        user = fetch_current_user_from_database()
+        g.user = user
+    return user
+
+@contextmanager
+def user_set(app, user):
+    def handler(sender, **kwargs):
+        g.user = user
+    with appcontext_pushed.connected_to(handler, app):
+        yield
+
+@app.route('/users/me')
+def users_me():
+    return jsonify(username=g.user.username)
+
+with user_set(app, my_user):
+    with app.test_client() as c:
+        resp = c.get('/users/me')
+        data = json.loads(resp.data)
+        self.assert_equal(data['username'], my_user.username)
+
+
+## Keeping the Context around
+
+with app.test_client() as c:
+    rv = c.get('/?tequila=42')
+    assert request.args['tequila'] == '42'
+
+
+## Accessing and Modifying Sessions
+
+with app.test_client() as c:
+    rv = c.get('/')
+    assert flask.session['foo'] == 42
+
+## Testing Json APIs
+
+@app.route('/api/auth')
+def auth():
+    json_data = request.get_json()
+    email = json_data['email']
+    password = json_data['password']
+    return jsonify(token=generate_token(email, password))
+
+with app.test_client() as c:
+    rv = c.post('/api/auth', json={
+        'email': 'flask@example.com', 'password': 'secret'
+    })
+    json_data = rv.get_json()
+    assert verify_token(email, json_data['token'])
+
+
+## Testing CLI command
+
+@app.cli.command('hello')
+@click.option('--name', default='World')
+def hello_command(name):
+    click.echo(f'Hello, {name}!')
+
+def test_hello():
+    runner = app.test_cli_runner()
+
+    # invoke the command directly
+    result = runner.invoke(hello_command, ['--name', 'Flask'])
+    assert 'Hello, Flask' in result.output
+
+    # or by name
+    result = runner.invoke(args=['hello'])
+    assert 'World' in result.output
+
+def upper(ctx, param, value):
+    if value is not None:
+        return value.upper()
+
+@app.cli.command('hello')
+@click.option('--name', default='World', callback=upper)
+def hello_command(name):
+    click.echo(f'Hello, {name}!')
+
+
+def test_hello_params():
+    context = hello_command.make_context('hello', ['--name', 'flask'])
+    assert context.params['name'] == 'FLASK'
